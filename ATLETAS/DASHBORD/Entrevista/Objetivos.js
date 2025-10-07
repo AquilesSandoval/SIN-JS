6e33cafaa410f9c739fe96edc2330663f1976943
allinyourmindyuc <allinyourmindyuc@gmail.com>
2025-10-07 16:33:28 -0600

    Updated Objetivos.js

---
+++ ATLETAS/DASHBORD/Entrevista/Objetivos.js
@@ -0,0 +1,15 @@
+jQuery(function ($) {
+jQuery('#w0').yiiActiveForm([], []);
+});
+
+function changeIdioma(id, flag) {
+    console.log(id + "-" + flag);
+    $.ajax({
+        url: '/web/index.php?r=idiomas/changeidioma',
+        type: "POST",
+        data: "id=" + id + "&flag=" + flag,
+        success: function(response) {
+            location.reload();
+        }
+    });
+}
