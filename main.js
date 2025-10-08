198ca0289c18f51d6bf43ebda8c948c3c60497fc
darwinlacabranunez-crypto <darwinlacabranunez@gmail.com>
2025-10-07 22:27:18 -0600

    Updated index.html

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-import $ from 'jquery';
-
-window.$ = window.jQuery = $;
-
-$(document).ready(function() {
+// jQuery is already loaded globally from the HTML script tags
+// Wait for jQuery to be available
+if (typeof $ !== 'undefined') {
     console.log('jQuery loaded and ready');
     console.log('Current page:', window.location.pathname);
 
+    // Load index.js for the home page
     if (window.location.pathname.includes('index.html') || window.location.pathname === '/') {
         import('./index.js').then(() => {
             console.log('index.js loaded');
@@ -13,4 +12,6 @@ $(document).ready(function() {
             console.error('Error loading index.js:', err);
         });
     }
-});
+} else {
+    console.error('jQuery not loaded');
+}
